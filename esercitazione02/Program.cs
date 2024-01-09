0ab83b1855f84659730a67708105640736e84342
Dy505yea <dylannzambrano@gmail.com>
2024-01-09 12:53:25 +0100

    Data di nascita nei codici 02

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -2,7 +2,8 @@
     {
         static void Main(string[] args)
         {
-            decimal altezza = 1.80m;
-            Console.WriteLine($"Sei alto {altezza} metri"); //$ è l'interpolazione di stringhe
+            //usually, c'è bisogno di una libreria a parte
+            DateTime DataDiNascita = new DateTime(1980, 1, 1);
+            Console.WriteLine($"Sei nato il {DataDiNascita}"); //$ è l'interpolazione di stringhe
         }
     }
