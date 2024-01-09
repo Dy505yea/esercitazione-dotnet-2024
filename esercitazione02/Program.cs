8088455894bfb9fce58f69e1b4a6d2cae869796e
Dy505yea <dylannzambrano@gmail.com>
2024-01-09 14:56:03 +0100

    ho messo il tema abissale :)

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -3,7 +3,7 @@
         static void Main(string[] args)
         {
             DateTime DataDiNascita = new DateTime(1980, 1, 1);
-            Console.WriteLine($"Sei nato il {DataDiNascita.ToShortDateString()}"); //$ è l'interpolazione di stringhe
+            Console.WriteLine($"Sei nato il {DataDiNascita.ToLongDateString()}"); //$ è l'interpolazione di stringhe
             //durante la scrittura, da quando uno mette il ".", li libreria proverà a suggerire un metodo già esistente
             //se provi a scrivere senza digitare il ".", non va più
         }
