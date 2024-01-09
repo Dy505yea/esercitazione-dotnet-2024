82c564b79abfed1c503b6de6f66303c3b7fba2b4
Dy505yea <dylannzambrano@gmail.com>
2024-01-09 14:39:34 +0100

    cosa potrebbe andar storto

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -5,5 +5,6 @@
             DateTime DataDiNascita = new DateTime(1980, 1, 1);
             Console.WriteLine($"Sei nato il {DataDiNascita.ToShortDateString()}"); //$ è l'interpolazione di stringhe
             //durante la scrittura, da quando uno mette il ".", li libreria proverà a suggerire un metodo già esistente
+            //se provi a scrivere senza digitare il ".", non va più
         }
     }
