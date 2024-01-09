02796f340233850a63d6f11dfc9ff5cff925fcfa
Dy505yea <dylannzambrano@gmail.com>
2024-01-09 14:14:09 +0100

    Data di nascita senza orario :)

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -2,8 +2,8 @@
     {
         static void Main(string[] args)
         {
-            //usually, c'è bisogno di una libreria a parte
             DateTime DataDiNascita = new DateTime(1980, 1, 1);
-            Console.WriteLine($"Sei nato il {DataDiNascita}"); //$ è l'interpolazione di stringhe
+            Console.WriteLine($"Sei nato il {DataDiNascita.ToShortDateString()}"); //$ è l'interpolazione di stringhe
+            //durante la scrittura, da quando uno mette il ".", li libreria proverà a suggerire un metodo già esistente
         }
     }
