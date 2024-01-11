08e96425f9d0b13b4e1a170855cf988fc11f3207
Dy505yea <dylannzambrano@gmail.com>
2024-01-11 10:40:50 +0100

    Liste 01

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -2,11 +2,13 @@
     {
         static void Main(string[] args)
         {
-            string[] nomi= new string[3];
-            nomi[0]="Mario";
-            nomi[1]="Luigi";
-            nomi[2]="Giovanni";
-            Console.WriteLine($"Buonaseeeeera   {nomi[0]}, {nomi[1]}, {nomi[2]}"); //$ è l'interpolazione di stringhe
-            Console.WriteLine($"Siete solo in {nomi.Length}?"); //il .Length accanto al nome dell'array, indica quanti elementi son contenuti
+            //<> è definito come diamond
+            List<string> nomi = new List<string>();
+            //ciò che è sottostante son metodi, non più assegnazioni
+            nomi.Add("Mario");
+            //infatti, gli elementi son inseriti tramite le parantesi, invece di =
+            nomi.Add("Luigi");
+            nomi.Add("Giovanni");
+            Console.WriteLine($"Buonaseeeeera   {nomi[0]}, {nomi[1]}, {nomi[2]}");
         }
     }
--- esercitazione02/obj/Debug/net8.0/esercitazione02.AssemblyInfo.cs
+++ esercitazione02/obj/Debug/net8.0/esercitazione02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("esercitazione02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a980d71fdb68aece323c19b76bde2ce5fb1bf90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b72e6e01c5a6e956764d3b92a1c6e05d818dbc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("esercitazione02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("esercitazione02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
