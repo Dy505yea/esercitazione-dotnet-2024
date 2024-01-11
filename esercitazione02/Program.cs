ff211c9c100c44e94039d8156f624cffd5e9ec30
Dy505yea <dylannzambrano@gmail.com>
2024-01-11 12:26:47 +0100

    Strutture in costruzione

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -2,13 +2,28 @@
     {
         static void Main(string[] args)
         {
-            //Una pila è come mettere delle biglie in un tubo su misura: la prima che metti sarà l'ultima che toglierai
-            Stack<string> nomi = new Stack<string>();
-            nomi.Push("Mario");
-            nomi.Push("Luigi");
-            nomi.Push("Giovanni");
-            //ed esattamente con questo concetto, l'unico modp per printare gli elementi è col seguente metodo
-            Console.WriteLine($"Buonaseeeeera   {nomi.Pop()}, {nomi.Pop()}, {nomi.Pop()}");
-            //L'ultimo elemento è il primo a venir fuori
+            //inizializzazione di 2 tipi d'array
+            string[] nomi= new string[] {"Mario", "Giovanni", "Salomone"};
+            int[] numeri= new int[] {4, 12, 62, 24, 2};
+            
+            Console.WriteLine($"Buonasera {nomi[0]}, {nomi[1]}, {nomi[2]}\n");
+            Console.WriteLine($"I vostri numeri: {numeri[0]}, {numeri[1]}, {numeri[2]}, {numeri[3]}, {numeri[4]}\n\n");
+
+            //inizializzazione di 2 tipi di liste
+            List<int> listaNumeri=new List<int> {8, 21, 444, 12, 90};
+            List<string> listaNomi=new List<string> {"Osvaldo", "Enrico", "Tommaso"};
+            
+            Console.WriteLine($"Buonasera {listaNomi[0]}, {listaNomi[1]}, {listaNomi[2]}\n");
+            Console.WriteLine($"I vostri numeri: {listaNumeri[0]}, {listaNumeri[1]}, {listaNumeri[2]}, {listaNumeri[3]}, {listaNumeri[4]}\n\n");
+
+            //inizializzione di una pila
+            Stack<int> pilaNumeri= new Stack<int> (new int[] {505, 31, 49, 98, 5});
+            
+            Console.WriteLine($"Ecco numeri i nascosti: {pilaNumeri.Pop()}, {pilaNumeri.Pop()}, {pilaNumeri.Pop()}, {pilaNumeri.Pop()}, {pilaNumeri.Pop()}\n");
+
+            //inizializzazione di una coda
+            Queue<string> codaNomi= new Queue<string> (new string[] {"Dario", "Silvio", "Agata"});
+            
+            Console.WriteLine($"Sono a nome di {codaNomi.Dequeue()}, {codaNomi.Dequeue()}, {codaNomi.Dequeue()}\n\n");
         }
     }
