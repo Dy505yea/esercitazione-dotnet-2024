76d2f49464dae56d96111a7b6a20a056991ba35b
Dy505yea <dylannzambrano@gmail.com>
2024-01-11 10:08:23 +0100

    Blueprint di strutture dati 03

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -2,19 +2,13 @@
     {
         static void Main(string[] args)
         {
-            string[] nomi= new string[3];
+            string[] nomi= new string[3];   //string è di per sé un array di stringhe, ma un array dinamico
+            //dal momento che si aggiunge [] subito accanto al tipo, rendendolo un array di array
+            //ma, a differenza di string, questo array è predeterminato, quindi necessità di sapere quanto spazio deve essere creato
             nomi[0]="Mario";
             nomi[1]="Luigi";
             nomi[2]="Giovanni";
-            Console.WriteLine($"Buonaseeeeera   {nomi[0]} {nomi[1]} {nomi[2]}!!!!!"); //$ è l'interpolazione di stringhe
-            //prova da qui
-            //prima prova, contenitore di singola stringa
-            //string cont= nomi[2];
-            //prova con una stringa singola
-            //char prova= cont;
-            //seconda prova: usare l'array di stringhe come se fosse una matrice
-            char prova= nomi[2][1]; //se si cerca un altro elemento di un array di stringhe, secondo teoria, dovrebbe uscire un carattere
-            Console.WriteLine($"{nomi[2]}, la seconda lettera del tuo nome è {prova}, nevvero?");
-            //fine prova
+            Console.WriteLine($"Buonaseeeeera   {nomi[0]}, {nomi[1]}, {nomi[2]}"); //$ è l'interpolazione di stringhe
+            Console.WriteLine($"Siete solo in {nomi.Length}?");
         }
     }
