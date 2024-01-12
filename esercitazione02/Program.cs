1e8d024b83f731fa06ae91b95645c353c8fea79b
Dy505yea <dylannzambrano@gmail.com>
2024-01-12 12:12:36 +0100

    Mario è a piede libero

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -15,20 +15,29 @@
             //posizione nell'array
             int pos=0;
             Console.WriteLine($"Ricerca di {wanted} in corso");
+            //ciclo foreach nell'array, letto è ciò che ritorna per orgni elemento
             foreach(string letto in nomi)
             {
+                //se l'abbiamo stanato
                 if(letto==wanted)
                 {
+                    //aumenta il contatore
                     conta++;
+                    //aggiungiamo il nome
                     laLista.Add(letto);
+                    //e lo mettiamo nel dizionario con il posto in cui sta
                     numeralo.Add(pos, letto);
                 }
+                //prossima linea
                 pos++;
             }
             Console.WriteLine($"Trovato {wanted} {conta} volta/e");
+            //ciclo per stampare un messaggio semplice semplice
             foreach(int chiave in numeralo.Keys)
             {
+                //chiave è il corrente key che si sta leggendo in tutto il dizionario
                 Console.WriteLine($"Nella linea {chiave}, è stato rilevato {numeralo[chiave]}");
+                //numeralo[chiave] è il corrente value di suddetta key
             }
         }
     }
