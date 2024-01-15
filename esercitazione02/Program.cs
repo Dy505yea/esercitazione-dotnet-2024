5bc7faad0170fd61fd5d8818e52842f7f4a100d6
Dy505yea <dylannzambrano@gmail.com>
2024-01-15 12:52:01 +0100

    command line molto molto basilare

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -1,39 +1,39 @@
 ﻿class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.BackgroundColor = ConsoleColor.DarkCyan;
+        Console.Clear(); // Pulisce la console ad ogni iterazione
+        Console.WriteLine("inizio di un programma\n");
+        string te= "Dylann";
+        while (true)
         {
-            //Preferneza personale, almeno ho controllo per l'inizio
-            Console.WriteLine($"Aspetto che te mi faccia partire");
-            Console.ReadKey();
-            Console.WriteLine($"\nInizializzo na cosetta... aspe, voglio dormire");
-            //Per liberarsi da sto ciclo, c'è bisogno di un break
-            while(true)
+            string input= Console.ReadLine();
+            Console.Write("\n");
+            if(input.StartsWith("cmd: "))
             {
-                Console.WriteLine($"\ns'hai bisogno di me, premi solo ed esclusivamente solo ctrl + n");
-                //inizializzazione della variabile tasto
-                ConsoleKeyInfo keyInfo=Console.ReadKey(true);
-                //a differenza di normali variabili, per fare un controllo qui si necessità la metà dei caratteri per la condizione
-                //credo il .Modifiers sia qualcosa come Ctrl, Alt e Maiusc, il consolmodifiers.control è Ctrl in specifico
-                //Comunque, sta attento, ci son un pò di combinazioni che, se non eseguiti da un terminale esterno, non lasciano leggere al programma
-                if((keyInfo.Modifiers & ConsoleModifiers.Control)!=0)
+                string comando=input.Substring(5);
+                switch(comando.ToLower())
                 {
-                    Console.WriteLine($"control l'hai messo");
-                    //Fa un controllo del tasto normale, in sto caso se hai premuto anche N
-                    if(keyInfo.Key==ConsoleKey.N)
-                    {
-                         Console.WriteLine("\nalmeno sai leggere");
-                         break;
-                     }
-                    else
-                    {
-                        Console.WriteLine($"\nSai dove sta la fricking N?!?");
-                    }
-                }
-                //se non ti fidi, un pò di scazzata ci vuole
-                else
-                {
-                     Console.WriteLine($"\nT'ho detto di non toccar altro");
+                    case "info":
+                        Console.WriteLine("Non ho informazioni ._ .");
+                        break;
+                    case "name":
+                        Console.WriteLine($"Te chiami {te}, cervello de pesce rosso");
+                        break;
+                    case "exit":
+                        Console.WriteLine("Addios");
+                        return;
+                    default:
+                        Console.WriteLine("Ma che me scrivi?\nSenti, dato che te non sai na ceppa, ecco a te i comandi:\n\ninfo\nname\nexit\nreprovaci\n");
+                        break;
                 }
             }
+            else
+            {
+                Console.WriteLine("E che vuoi che faccia se non mi dai un comando?!? (per la cronaca, ho solo 'cmd:')");
+            }
+            Console.WriteLine("*s'accende na siga*\n");
         }
     }
+}
