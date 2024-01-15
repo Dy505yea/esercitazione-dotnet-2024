4df17a92a9bb1ee49060612ef0e819d9772d9be7
Dy505yea <dylannzambrano@gmail.com>
2024-01-15 09:44:48 +0100

    yet da provare

--- esercitazione02/Program.cs
+++ esercitazione02/Program.cs
@@ -2,15 +2,15 @@
     {
         static void Main(string[] args)
         {
-            List<string> nomi=new List<string>();
-            nomi.Add("Rossi");
-            nomi.Add("Luigi");
-            nomi.Add("Lorenzo");
-            int i=0;
-            while(i<nomi.Count)
-            {
-                Console.WriteLine($"Ciao {nomi[i]}");
-                i++;
-            }
+           Console.WriteLine($"Fermami con N se ci riesci");
+           while(true)
+           {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if(keyInfo.Key==ConsoleKey.N)
+                {
+                    Console.WriteLine($"darn...");
+                    break;
+                }
+           }
         }
     }
