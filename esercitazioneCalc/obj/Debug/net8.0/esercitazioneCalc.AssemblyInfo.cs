09af45186a12df44a1c344ce9dd4107a5ec91de5
Dy505yea <dylannzambrano@gmail.com>
2024-01-26 15:16:42 +0100

    mi dite i vari siti

--- esercitazioneCalc/obj/Debug/net8.0/esercitazioneCalc.AssemblyInfo.cs
+++ esercitazioneCalc/obj/Debug/net8.0/esercitazioneCalc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("esercitazioneCalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ac3f245d9a1c80b0a333bb18671d823d1c9b31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6aed27d0af33f731f6e4bd4db1451f323496e5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("esercitazioneCalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("esercitazioneCalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
