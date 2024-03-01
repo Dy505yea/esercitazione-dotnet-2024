0285f9d0ae879ff8c4c5f35af2876438e509dca3
Dy505yea <dylannzambrano@gmail.com>
2024-03-01 15:52:03 +0100

    b

--- esercitazione03/database through sql/scrFirsTime.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE categorie (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT UNIQUE);
-CREATE TABLE merce (id INTEGER PRIMARY KEY AUTOINCREMENT, nomeSpec TEXT UNIQUE, nomeGen TEXT, prezzo REAL, quantita INTEGER CHECK (quantita>=0), id_categoria INTEGER, FOREIGN KEY (id_categoria) REFERENCES categorie(id));
-INSERT INTO categorie VALUES (NULL, 'Aerofoni'), (NULL, 'Cordofoni'), (NULL, 'Membranofoni'), (NULL,  'Idiofoni'), (NULL, 'Miscellaneous');
